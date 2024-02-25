b2306bce3bbb03b11ff022dafe7738aa657843bb
Alex <abondarenko101@gmail.com>
2024-02-25 13:16:11 -0800

    Added <gen> and <each> tags

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,4 +1,5 @@
 const readline = require("readline");
+const fs = require('fs').promises;
 
 const { OpenAI } = require("openai");
 const prompts = require('@inquirer/prompts');
@@ -143,17 +144,18 @@ function parseParam(node) {
             if (typeof elem === 'object' && (elem.tag === 'p' || elem.tag === 'opt')) {
                 item.properties[elem.attr.name] = parseParam(elem);
                 if (elem.tag === 'p') {
-                    if (!item.hasOwnProperty('required')) item.required = [];
                     item.required.push(elem.attr.name);
                 }
             }
         }
     } else {
         for (const attr in node.attr) {
-            if (attr !== 'd' && attr !== 'action' && attr !== 'array' && attr !== 'name') {
-                item[attr] = node.attr[attr];
-            } else {
+            if (['minItems', 'maxItems', 'minLength', 'maxLength'].includes(attr)) {
+                item[attr] = parseInt(node.attr[attr], 10);
+            } else if (attr === 'd') {
                 item.description = node.attr.d;
+            } else if (!['d', 'action', 'array', 'name'].includes(attr)) {
+                item[attr] = node.attr[attr];
             }
         }
         if (!item.hasOwnProperty('type')) {
@@ -174,7 +176,6 @@ function parseParam(node) {
 async function evaluateChat(nodes, parentScope, context) {
     const messages: any[] = [];
     const tools: any[] = [];
-    const toolActions: any = {};
     const scope = {parent: parentScope, bindings: {}};
 
     for (const node of nodes) {
@@ -184,7 +185,6 @@ async function evaluateChat(nodes, parentScope, context) {
         switch (node.tag) {
             case 'system': 
             case 'user':
-                console.log(node);
                 const content = replaceVariables(node.elem[0].trim().replace('\t', ''), scope);
                 messages.push({ role: node.tag, content });
                 break;
@@ -199,7 +199,6 @@ async function evaluateChat(nodes, parentScope, context) {
                 if (param.type !== 'object') {
                     param = {type: 'object', properties: map1('input', param), required: ['input']};
                 }
-                toolActions[node.name] = node.action;
                 tools.push({
                     type: 'function',
                     function: {
@@ -211,70 +210,7 @@ async function evaluateChat(nodes, parentScope, context) {
                 break;
             
             case 'response':
-                let done = false;
-                console.log(JSON.stringify(tools, null, 2));
-                while (!done) {
-                    done = true; // might get overridden
-                    let response;
-                    if (tools.length > 0) {
-                        console.log(messages);
-                        response = await context.openai.chat.completions.create({
-                            model: "gpt-3.5-turbo-1106",
-                            messages,
-                            tools: tools,
-                            tool_choice: "auto", // auto is default, but we'll be explicit
-                        });
-                        messages.push({
-                            role: "assistant",
-                            tool_calls: response.choices[0].message.tool_calls,
-                        })
-                        for (const call of response.choices[0].message.tool_calls) {
-                            const args = JSON.parse(call.function.arguments);
-                            const callback = context.callbacks[call.function.name];
-                            const res = await callback(args, context);
-                            
-                            // TODO: confirm
-                            let content;
-                            if (res.action === "loop") {
-                                content = res.content;
-                                done = false;
-                            } else if (res.action === "feedback") {
-                                console.log(res.content);
-                                const answer = await context.input("Review the output and see if it matches your expectations. Enter your feedback, or skip to accept.");
-                                if (answer.trim() === "") {
-                                    content = "Accepted";
-                                } else {
-                                    content = answer;
-                                    done = false;
-                                }
-                            } else if (res.action === "noop") {
-                                return;
-                            }
-                            messages.push({
-                                tool_call_id: call.id,
-                                role: "tool",
-                                name: call.function.name,
-                                content,
-                            });
-                        }
-                    } else {
-                        response = await context.openai.chat.completions.create({
-                            model: "gpt-3.5-turbo-1106",
-                            messages,
-                        });
-                        messages.push({
-                            role: "assistant",
-                            content: response.choices[0].message.content,
-                        })
-                        console.log(response.choices[0].message);
-                    }
-                    console.log({
-                        created: response.created,
-                        model: response.model,
-                        usage: response.usage,
-                        choices: response.choices,
-                    });
-                }
+                await chatLoop(messages, tools, context, context.callbacks);
                 break;
         }
     }
@@ -284,11 +220,215 @@ async function evaluateChat(nodes, parentScope, context) {
 }
 
 
+async function chatLoop(messages: any[], tools: any[], context, callbacks, model: string = "gpt-3.5-turbo-1106") {
+    let done = false;
+    console.log(JSON.stringify(tools, null, 2));
+    while (!done) {
+        done = true; // might get overridden
+        let response;
+        if (tools.length > 0) {
+            console.log(messages);
+            response = await context.openai.chat.completions.create({
+                model: model,
+                messages,
+                tools: tools,
+                tool_choice: "auto", // auto is default, but we'll be explicit
+            });
+            console.log({
+                created: response.created,
+                model: response.model,
+                usage: response.usage,
+                choices: response.choices,
+            });
+            messages.push({
+                role: "assistant",
+                tool_calls: response.choices[0].message.tool_calls,
+            })
+            const calls = response.choices[0].message.tool_calls.map((call) => {
+                return {
+                    id: call.id,
+                    name: call.function.name,
+                    arguments: JSON.parse(call.function.arguments),
+                };
+            });
+            console.log(JSON.stringify(calls, null, 2));
+            for (const call of calls) {
+                if (!callbacks.hasOwnProperty(call.name)) {
+                    messages.push({
+                        tool_call_id: call.id,
+                        role: "tool",
+                        name: call.name,
+                        content: `${call.name} is not one of the provided tools`,
+                    });
+                    return;
+                }
+                const res = await callbacks[call.name](call.arguments, context);
+                
+                // TODO: confirm
+                let content;
+                if (res.action === "loop") {
+                    content = res.content;
+                    done = false;
+                } else if (res.action === "feedback") {
+                    console.log(call.arguments);
+                    const answer = await context.input("Review the output and see if it matches your expectations. Enter your feedback, or skip to accept.");
+                    if (answer.trim() === "") {
+                        content = "Accepted";
+                        if (res.success) res.success();
+                    } else {
+                        content = answer;
+                        done = false;
+                        if (res.failure) res.failure();
+                    }
+                } else if (res.action === "noop") {
+                    return;
+                }
+                messages.push({
+                    tool_call_id: call.id,
+                    role: "tool",
+                    name: call.name,
+                    content,
+                });
+            }
+        } else {
+            response = await context.openai.chat.completions.create({
+                model: model,
+                messages,
+            });
+            console.log({
+                created: response.created,
+                model: response.model,
+                usage: response.usage,
+                choices: response.choices,
+            });
+            messages.push({
+                role: "assistant",
+                content: response.choices[0].message.content,
+            })
+            console.log(response.choices[0].message);
+        }
+    }
+}
+
+
+function parseGen(node, scope) {
+    const messages: any[] = [];
+
+    let feedback: boolean = false;
+    for (const elem of node.elem) {
+        if (typeof elem === 'string') {
+            continue;
+        }
+        switch (elem.tag) {
+            case 'system': 
+            case 'user':
+                const content = replaceVariables(elem.elem[0].trim().replace('\t', ''), scope);
+                messages.push({ role: elem.tag, content });
+                break;
+            
+            case 'feedback':
+                feedback = true;
+                break;
+        }
+    }
+
+    let param = parseParam(node);
+    let singleton = false;
+    if (param.type !== 'object') {
+        param = {type: 'object', properties: map1('input', param), required: ['input']};
+        singleton = true;
+    }
+    const tools = [
+        {
+            type: 'function',
+            function: {
+                name: node.attr.store,
+                parameters: param,
+            },
+        }
+    ];
+
+    return {
+        messages,
+        tools,
+        feedback,
+        singleton,
+    }
+}
+
+
+async function evaluateGen(node, scope, context) {
+    const {
+        messages,
+        tools,
+        feedback,
+    } = parseGen(node, scope);
+    
+    const label = node.attr.store;
+    const callbacks = map1(label, async (args, ctx) => {
+        const store = () => {
+            console.log(label);
+            console.log(args);
+            scope.bindings[label] = args;
+        };
+        if (!feedback) {
+            store();
+            return {
+                action: 'noop',
+            }
+        } else {
+            return {
+                action: 'feedback',
+                success: store,
+            }
+        }
+    });
+    await chatLoop(messages, tools, context, callbacks);
+
+    return {};
+}
+
+
+async function evaluateEach(node, parentScope, context) {
+    console.log(parentScope);
+    const label = node.attr.store;
+    const over = lookup(parentScope, node.attr.in);
+    if (over === undefined) {
+        throw new Error(`could not find '${node.attr.in}' in scope`);
+    }
+    for (const each of over.input) {
+        const scope = {parent: parentScope, bindings: each};
+        const {
+            messages,
+            tools,
+            feedback,
+        } = parseGen(node, scope);
+
+        const callbacks = map1(label, async (args, ctx) => {
+            const store = () => each[label] = args;
+            if (!feedback) {
+                store();
+                return {
+                    action: 'noop',
+                }
+            } else {
+                return {
+                    action: 'feedback',
+                    success: store,
+                }
+            }
+        });
+        await chatLoop(messages, tools, context, callbacks);
+    }
+
+    return {};
+}
+
+
 // TODO: template and sub
 // TODO: loop
 // TODO: task
 // TODO: revise
-// TODO: gen
 // TODO: plugin
 // TODO: complete
 // TODO: configure model / temperature
@@ -306,7 +446,15 @@ async function evaluateHelper(nodes, parentScope, context) {
                 break;
             
             case 'chat':
-                evaluateChat(node.elem, scope, context);
+                await evaluateChat(node.elem, scope, context);
+                break;
+            
+            case 'gen':
+                await evaluateGen(node, scope, context);
+                break;
+            
+            case 'each':
+                await evaluateEach(node, scope, context);
                 break;
         }
     }
@@ -316,17 +464,17 @@ async function evaluateHelper(nodes, parentScope, context) {
 }
 
 
-function parseXml(src) {
+export function parseXml(src) {
     return new xml.DOMParser().parseFromString(src, "text/xml")
 }
 
 
-function dumpXml(doc) {
-    return new xml.XMLSerializer().serializeToString(xmlDocR);
+export function dumpXml(doc) {
+    return new xml.XMLSerializer().serializeToString(doc);
 }
 
 
-async function evaluate(node) {
+export async function evaluate(node) {
     const { openai: { token }, checkvist } = await readYamlFile('config.yaml');
     const ctx = {
         openai: new OpenAI({ apiKey: token }),
@@ -345,27 +493,31 @@ async function evaluate(node) {
                 if (!args.complete) {
                     return {
                         action: 'noop',
-                        content: args,
                     }
                 } else {
                     return {
                         action: 'feedback',
-                        content: args,
                     }
                 }
             },
         },
     }
     
-    await evaluateHelper(node.elem, {parent: null, bindings: {}}, ctx);
+    return await evaluateHelper(node.elem, {parent: null, bindings: {}}, ctx);
 }
 
 
-async function read(src: string) {
+function read(src: string) {
     return xmlToJson(parseXml(src).documentElement);
 }
 
 
-async function readEvaluatePrint(src: string) {
-    evaluate(read(src));
+export async function readEvaluatePrint(src: string) {
+    console.log(await evaluate(read(src)));
+}
+
+
+export async function executeFile(path: string) {
+    const src = await fs.readFile(path, 'utf8');
+    await readEvaluatePrint(src);
 }
